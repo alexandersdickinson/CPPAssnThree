bc27f0371c6ac8849b4f9c5638c688972841f507
Alexander S. Dickinson <alexandersdickinson@gmail.com>
2016-02-15 15:59:40 -0800

    Initial commit.

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  paycheck
+//
+//  Created by Alex Dickinson on 15.02.16.
+//  Copyright © 2016 Alex Dickinson. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
